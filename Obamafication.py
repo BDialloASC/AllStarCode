515774763b70878a75c24973d33afb46a286584a
BDialloASC <dialob1@bxscience.edu>
2016-07-18 09:26:42 -0400

    Obamafication Project
    obamafies a picture - added comments

--- Obamafication.py
+++ Obamafication.py
@@ -1,17 +1,18 @@
 #Obamafication
 from Myro import *
 
+#"Obamafies" a picture based on the color of each pixel
 def obamafication():
-    picture = pickAFile()
-    pic = makePicture(picture)
-    pixels = getPixels(pic)
-    ObamaDarkBlue = makeColor(0,51,76)
-    ObamaRed = makeColor(217, 26, 33)
+    picture = pickAFile() #Opens a window each time to choose image file
+    pic = makePicture(picture) #Save picture in a variable
+    pixels = getPixels(pic)    #saves list of pixels in a variable
+    ObamaDarkBlue = makeColor(0,51,76)       #Obama color values that pixels 
+    ObamaRed = makeColor(217, 26, 33)        #pixels will be converted to
     ObamaBlue = makeColor(112,150,158)
     ObamaYellow = makeColor(252, 227, 166)
 
-    for pxl in pixels:
-        grey = getGray(pxl)
+    for pxl in pixels:  #for each pixel in the pic
+        grey = getGray(pxl)           #conditionals to change each pixel 
         if(grey>180):
             setColor(pxl,ObamaYellow)
         elif(grey>120):
@@ -21,7 +22,7 @@ def obamafication():
         else:
             setColor(pxl,ObamaDarkBlue)
 
-    show(pic)
+    show(pic) #show new obamafied picture
 
 
 
